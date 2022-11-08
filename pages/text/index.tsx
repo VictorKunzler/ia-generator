76280857c7d458af8173e8100ee89bece17c8182
Victor Kunzler <victor_kunzler@hotmail.com>
2022-11-08 10:16:45 -0300

    initial structure

--- pages/text/index.tsx
+++ pages/text/index.tsx
@@ -1,9 +1,13 @@
+import styles from '../../styles/TextGenerator.module.css'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function TextGenerator() {
   return (
+    <>
+      <Head>
+        <title>Text Generator</title>
+      </Head>
+
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <span className={styles.blueText}>IA Generator</span>
@@ -14,11 +18,12 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="/text" className={styles.card}>
+          <a href="" className={styles.card}>
             <h2>Text &rarr;</h2>
             <p>Start your history and let the IA complete it for you</p>
           </a>
         </div>
       </main>
+    </>
   )
 }
